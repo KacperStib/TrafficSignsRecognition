f6d1299ed75e546a5b7572ad1e1de34e5950ebaa
Kacper <ks300578@student.polsl.pl>
2024-06-22 14:16:49 +0200

    comments-and-optimazed-python

--- scripts/cords.py
+++ scripts/cords.py
@@ -1,5 +1,6 @@
 
 def Analyze(path):
+    #import bilbiotek oraz modelu do rozpoznawania znakow
     import cv2
     from roboflow import Roboflow
     rf = Roboflow (api_key="Ox9EnqPo6BpmOWSX9sTr") #(api_key="Ox9EnqPo6BpmOWSX9sTr")
@@ -9,26 +10,15 @@ def Analyze(path):
     img = cv2.imread(path,0)
 
     predictions = model.predict(img, confidence=70, overlap=40).json()
-
+    
+    #obliczenie wspolrzednych bounding boxa
     for bounding_box in predictions['predictions']:
         x0 = bounding_box['x'] - bounding_box['width'] / 2
         x1 = bounding_box['x'] + bounding_box['width'] / 2
         y0 = bounding_box['y'] - bounding_box['height'] / 2
         y1 = bounding_box['y'] + bounding_box['height'] / 2
         
-        start_point = (int(x0), int(y0))
-        end_point = (int(x1), int(y1))
-        cv2.rectangle(img, start_point, end_point, color=(0,0,255), thickness=1)
-        
-        cv2.putText(
-            img,
-            bounding_box["class"],
-            (int(x0), int(y0) - 10),
-            fontFace = cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale = 0.6,
-            color = (255, 255, 255),
-            thickness=2
-        )
+    #zwrocenie wspolrzednych bounding boxa
     return [int(x0),int(y1),int(x1),int(y0)]
 #cv2.imshow("example_with_bounding_boxes.jpg", img)
 #cv2.waitKey(0) # waits until a key is pressed
--- scripts/name.py
+++ scripts/name.py
@@ -1,47 +1,37 @@
 
 def Analyze(path):
+    #import bilbiotek oraz modelu do rozpoznawania znakow
     import cv2
     from roboflow import Roboflow
-    rf = Roboflow (api_key="Ox9EnqPo6BpmOWSX9sTr") #(api_key="Ox9EnqPo6BpmOWSX9sTr")
+    rf = Roboflow (api_key="Ox9EnqPo6BpmOWSX9sTr") 
     project = rf.workspace().project("zdjecia-drogowe")
     model = project.version(6).model
     #img = "C:\\Users\\kacpe\\Desktop\\OSP\\dataset\\g6.jpg"
     img = cv2.imread(path,0)
-
     predictions = model.predict(img, confidence=70, overlap=40).json()
 
-    for bounding_box in predictions['predictions']:
-        x0 = bounding_box['x'] - bounding_box['width'] / 2
-        x1 = bounding_box['x'] + bounding_box['width'] / 2
-        y0 = bounding_box['y'] - bounding_box['height'] / 2
-        y1 = bounding_box['y'] + bounding_box['height'] / 2
+    #obliczenie wspolrzednych bounding boxa
+    for predictions in predictions['predictions']:
+        x0 = predictions['x'] - predictions['width'] / 2
+        x1 = predictions['x'] + predictions['width'] / 2
+        y0 = predictions['y'] - predictions['height'] / 2
+        y1 = predictions['y'] + predictions['height'] / 2
         
-        start_point = (int(x0), int(y0))
-        end_point = (int(x1), int(y1))
-        cv2.rectangle(img, start_point, end_point, color=(0,0,255), thickness=1)
-        
-        cv2.putText(
-            img,
-            bounding_box["class"],
-            (int(x0), int(y0) - 10),
-            fontFace = cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale = 0.6,
-            color = (255, 255, 255),
-            thickness=2
-        )
-    if bounding_box["class"] == "zielone":
-        bounding_box["class"] = "Zielone swiatlo"
-    elif bounding_box["class"] == "czerwone":
-        bounding_box["class"] = "Czerwone swiatlo"
-    elif bounding_box["class"] == "ust":
-        bounding_box["class"] = "Znak ustap pierwszenstwa"
-    elif bounding_box["class"] == "pierw":
-        bounding_box["class"] = "Znak drogi z pierwszenstwem"
-    elif bounding_box["class"] == "stop":
-        bounding_box["class"] = "Znak Stop"
+    #zamiana skrotu na odpowiednia reprezentatywna nazwe znaku
+    if predictions["class"] == "zielone":
+        predictions["class"] = "Zielone swiatlo"
+    elif predictions["class"] == "czerwone":
+        predictions["class"] = "Czerwone swiatlo"
+    elif predictions["class"] == "ust":
+        predictions["class"] = "Znak ustap pierwszenstwa"
+    elif predictions["class"] == "pierw":
+        predictions["class"] = "Znak drogi z pierwszenstwem"
+    elif predictions["class"] == "stop":
+        predictions["class"] = "Znak Stop"
 
-    return bounding_box["class"]
-#cv2.imshow("example_with_bounding_boxes.jpg", img)
+    #zwrocenie nazwy
+    return predictions["class"]
+#cv2.imshow("example_with_predictionses.jpg", img)
 #cv2.waitKey(0) # waits until a key is pressed
 #cv2.destroyAllWindows() # destroys the window showing image
 #print(Analyze("C:\\Users\\kacpe\\Desktop\\OSP\\dataset\\g6.jpg"))
